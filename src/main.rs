dfda475b85bd9ea272fe652cf2353add228b207e
Pyrode <pyrode.01@gmail.com>
2025-12-23 11:09:35 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::{fs::File, io::{Error, Read}};
+
+fn main() -> Result<(), Error> {
+    let mut file = File::open("")?;
+    let mut buffer = Vec::new();
+
+    file.read_to_end(&mut buffer)?;
+
+    Ok(())
+}
