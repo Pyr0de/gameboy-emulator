36197115fa544775da60308dde6e7f37404703ca
Pyrode <pyrode.01@gmail.com>
2025-12-27 13:02:31 +0530

    feat: Running a few instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -7,13 +7,35 @@ impl CPU {
         CPU(Registers::new())
     }
 
-    pub(crate) fn run_instruction(&mut self, memory: MemoryMapping) {
+    pub(crate) fn run_instruction(&mut self, memory: &MemoryMapping) {
         let instruction = memory[self.0.pc];
+        println!("{:x} {:x}",self.0.pc, instruction);
+        self.0.pc += 1;
         match instruction {
             0x0 => {},
+            0x0e => {
+                let data = self.get_u8(memory);
+                self.0.c = data;
+            }
+            0x21 => {
+                let data = self.get_u16(memory);
+                self.0.set_hl(data);
+            },
+            0x47 => self.0.b = self.0.a,
+            0xc3 => {
+                let addr = self.get_u16(memory);
+                self.0.pc = addr;
+            }
+            _ => println!("not implemented: {:x}", instruction)
+        };
+    }
 
-            _ => { println!("not implemented: {:x}", instruction) }
-        }
-
+    fn get_u16(&mut self, memory: &MemoryMapping) -> u16 {
+        self.0.pc += 2;
+        memory[self.0.pc - 2] as u16 | ((memory[self.0.pc - 1] as u16) << 8)
+    }
+    fn get_u8(&mut self, memory: &MemoryMapping) -> u8 {
+        self.0.pc += 1;
+        memory[self.0.pc - 1]
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,20 @@ mod cpu;
 
 use std::{fs::File, io::{Error, Read}};
 
+use crate::{cpu::CPU, memory_mapping::{MemoryMapping, ROM}};
+
 fn main() -> Result<(), Error> {
-    let mut file = File::open("")?;
+    let mut file = File::open("tests/roms/halt_bug.gb")?;
     let mut buffer = Vec::new();
 
     file.read_to_end(&mut buffer)?;
+    let memory = MemoryMapping{ ROM: ROM{rom: buffer} };
+
+    let mut cpu = CPU::new();
+
+    for _ in 0..10 {
+        cpu.run_instruction(&memory);
+    }
 
     Ok(())
 }
