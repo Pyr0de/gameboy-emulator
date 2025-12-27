5b3b2c2a1e6005a7039a6351ac5b8b36345e962c
Pyrode <pyrode.01@gmail.com>
2025-12-27 21:11:34 +0530

    feat: Added stack

--- src/memory_mapping.rs
+++ src/memory_mapping.rs
@@ -1,18 +1,38 @@
-use std::ops::Index;
+use std::ops::{Index, IndexMut};
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 pub(crate) struct MemoryMapping {
     pub ROM: ROM,
     // RAM,
     // VRAM,
+    pub stack: [u8; 0x7F]
 }
+
 
 impl Index<u16> for MemoryMapping {
     type Output = u8;
     fn index(&self, index: u16) -> &Self::Output {
         match index {
-            0x0..0x8000 => {
+            0x0..=0x7FFF => {
                 &self.ROM[index]
+            }
+            0xFF80..=0xFFFE => {
+                &self.stack[index as usize - 0xFF80]
+            }
+            _ => {
+                unimplemented!()
+            }
+        }
+    }
+}
+impl IndexMut<u16> for MemoryMapping {
+    fn index_mut(&mut self, index: u16) -> &mut Self::Output {
+        match index {
+            0x0..=0x7FFF => {
+                panic!("cannot write to rom: {:x}", index);
+            }
+            0xFF80..=0xFFFE => {
+                &mut self.stack[index as usize - 0xFF80]
             }
             _ => {
                 unimplemented!()
